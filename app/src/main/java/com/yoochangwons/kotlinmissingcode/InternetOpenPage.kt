d2298b9dd62d40782180d6e6576957c3af64813d
CN <ckddnjs554@naver.com>
2021-07-03 15:35:03 +0900

    Feat : addTextChangedListener 리스너 기능 추가해서 Log 로 확인한느 코드

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/InternetOpenPage.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/InternetOpenPage.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import com.yoochangwons.kotlinmissingcode.databinding.ActivityInternetOpenPageBinding
 
 class InternetOpenPage : AppCompatActivity() {
@@ -22,5 +25,21 @@ class InternetOpenPage : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(internetUri))
             startActivity(intent)
         }
+
+        // EditText 의 콜백을 감지하는 리스너
+        binding.uri.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("edit", "beforeTextChanged : " + s)
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("edit", "onTextChanged : " + s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s.toString() == "abc") Log.d("edit", "text is abc")
+                Log.d("edit", "afterTextChanged : " + s)
+            }
+        })
     }
 }
