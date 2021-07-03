a8bc38c24c98f35fab20ba4598a710b5858e1362
CN <ckddnjs554@naver.com>
2021-07-03 16:35:49 +0900

    Feat : Fragment 구현을 동적으로 하는 방법 코드 추가

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/FragmentActivity.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/FragmentActivity.kt
@@ -3,6 +3,7 @@ package com.yoochangwons.kotlinmissingcode
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.FragmentManager
 import com.yoochangwons.kotlinmissingcode.databinding.ActivityFragmentBinding
 
 class FragmentActivity : AppCompatActivity() {
@@ -15,6 +16,22 @@ class FragmentActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         Log.d("life_cycle", "onCreate")
+
+        binding.btn.setOnClickListener {
+            // 프라그먼트를 동적으로 작동하는 방법
+            val fragmentOne: FragmentOne = FragmentOne()
+            val fragmentManager: FragmentManager = supportFragmentManager
+
+            // Transaction
+            // 작업의 단위 -> 시작과 끝이 있다
+            val fragmentTransaction = fragmentManager.beginTransaction() // 시작
+            fragmentTransaction.replace(R.id.container, fragmentOne)
+
+            // 끝을 내는 방법 2가지
+            // commit() -> 시간 될 때 해 (좀더 안정적)
+            // commitNow() -> 지금 당장해
+            fragmentTransaction.commit()
+        }
     }
 
     override fun onStart() {
