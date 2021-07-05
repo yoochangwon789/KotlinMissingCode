62b595b5ccd698f9fa8c40e67b66c6eb099d9bc7
CN <ckddnjs554@naver.com>
2021-07-05 16:55:34 +0900

    Feat : Glide 이미지 로드 코드

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/LibraryActivity.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/LibraryActivity.kt
@@ -2,6 +2,7 @@ package com.yoochangwons.kotlinmissingcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.yoochangwons.kotlinmissingcode.databinding.ActivityLibraryBinding
 
 class LibraryActivity : AppCompatActivity() {
@@ -13,5 +14,9 @@ class LibraryActivity : AppCompatActivity() {
         binding = ActivityLibraryBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        Glide.with(this)
+            .load("https://file.mk.co.kr/meet/neds/2021/04/image_readtop_2021_330747_16177500644599916.jpg")
+            .into(binding.glide)
     }
 }
