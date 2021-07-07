06224da7ae4bb99f539f634c986d69d4117fcc33
CN <ckddnjs554@naver.com>
2021-07-07 23:43:04 +0900

    Feat : TabLayout 구현 코드 작성 pager 코드 구현은 아직 전단계

---
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/Fragment2.kt
@@ -0,0 +1,18 @@
+package com.yoochangwons.kotlinmissingcode
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
--- app/src/main/java/com/yoochangwons/kotlinmissingcode/TabPagerActivity.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/TabPagerActivity.kt
@@ -2,10 +2,49 @@ package com.yoochangwons.kotlinmissingcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.yoochangwons.kotlinmissingcode.databinding.ActivityTabPagerBinding
 
 class TabPagerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTabPagerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tab_pager)
+        binding = ActivityTabPagerBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("ONE"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("TWO"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Three"))
+    }
+}
+
+class PagerAdapter(
+    fragmentManager: FragmentManager,
+    val tabCount: Int
+) : FragmentStatePagerAdapter(fragmentManager) {
+
+    override fun getCount(): Int {
+        return tabCount
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when (position) {
+            0 -> {
+                return Fragment1()
+            }
+            1 -> {
+                return Fragment2()
+            }
+            2 -> {
+                return Fragment3()
+            }
+            else -> return Fragment1()
+        }
     }
 }
