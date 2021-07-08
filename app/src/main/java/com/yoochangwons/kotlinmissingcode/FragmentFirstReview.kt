9ced148fa07c8ce17e379b39c645ac7cae3097a8
CN <ckddnjs554@naver.com>
2021-07-08 17:31:46 +0900

    Feat : FragmentFirstReview 코드에서 FragmentTodoList 데이터 클래스, Recycler 기본 셋팅 코드 작성

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/FragmentFirstReview.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/FragmentFirstReview.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.yoochangwons.kotlinmissingcode.databinding.FragmentFirstReviewBinding
+import com.yoochangwons.kotlinmissingcode.databinding.FragmentFirstReviewItemBinding
 
 class FragmentFirstReview : Fragment() {
 
@@ -32,4 +34,26 @@ class FragmentFirstReview : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+}
+
+data class FragmentTodoList(val text: String, val isDone: Boolean)
+
+class FragmentFirstRecyclerViewAdapter(
+    private val dataSet: Array<String>
+) : RecyclerView.Adapter<FragmentFirstRecyclerViewAdapter.ViewHolder>() {
+
+    class ViewHolder(val itemViewBinding: FragmentFirstReviewItemBinding) : RecyclerView.ViewHolder(itemViewBinding.root) {}
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.fragment_first_review_item, viewGroup, false)
+
+        return ViewHolder(FragmentFirstReviewItemBinding.bind(view))
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = dataSet.size
 }
