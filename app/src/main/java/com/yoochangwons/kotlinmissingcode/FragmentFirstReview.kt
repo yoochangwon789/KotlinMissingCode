55bc894b8def6a550f14fe444e27e5809b178c8e
CN <ckddnjs554@naver.com>
2021-07-08 18:07:49 +0900

    Feat : FragmentFirstReview 코드에서 addTodoList 구현과 RecyclerView 와 fragment 연동

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/FragmentFirstReview.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/FragmentFirstReview.kt
@@ -1,10 +1,13 @@
 package com.yoochangwons.kotlinmissingcode
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.yoochangwons.kotlinmissingcode.databinding.FragmentFirstReviewBinding
 import com.yoochangwons.kotlinmissingcode.databinding.FragmentFirstReviewItemBinding
@@ -14,6 +17,8 @@ class FragmentFirstReview : Fragment() {
     private var _binding: FragmentFirstReviewBinding? = null
     private val binding get() = _binding!!
 
+    private val fragmentFirstTodoArrayList = ArrayList<FragmentTodoList>()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,25 +26,39 @@ class FragmentFirstReview : Fragment() {
     ): View? {
         _binding = FragmentFirstReviewBinding.inflate(inflater, container, false)
         val view = binding.root
+
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.frFirstRecyclerView.apply {
+            adapter = FragmentFirstRecyclerViewAdapter(fragmentFirstTodoArrayList)
+            layoutManager = LinearLayoutManager(activity)
+        }
 
+        binding.frFirstButton.setOnClickListener {
+            fragmentAddTodoList()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun fragmentAddTodoList() {
+        val text = binding.frFirstEditText.text.toString()
+        fragmentFirstTodoArrayList.add(FragmentTodoList(text))
+        binding.frFirstRecyclerView.adapter?.notifyDataSetChanged()
+    }
 }
 
-data class FragmentTodoList(val text: String, val isDone: Boolean)
+data class FragmentTodoList(val text: String, val isDone: Boolean = false)
 
 class FragmentFirstRecyclerViewAdapter(
-    private val dataSet: Array<String>
+    private val dataSet: List<FragmentTodoList>
 ) : RecyclerView.Adapter<FragmentFirstRecyclerViewAdapter.ViewHolder>() {
 
     class ViewHolder(val itemViewBinding: FragmentFirstReviewItemBinding) : RecyclerView.ViewHolder(itemViewBinding.root) {}
@@ -52,8 +71,10 @@ class FragmentFirstRecyclerViewAdapter(
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
+        viewHolder.itemViewBinding.frFirstTextViewItem.text = dataSet[position].text
     }
 
-    override fun getItemCount() = dataSet.size
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
 }
