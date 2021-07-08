c39cd4a2723739e2b31a7e2fd16dc1e6aace3cda
CN <ckddnjs554@naver.com>
2021-07-08 20:21:12 +0900

    Feat : FragmentSecondReview RecyclerView 초기 구현

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/FragmentSecondReview.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/FragmentSecondReview.kt
@@ -5,7 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.yoochangwons.kotlinmissingcode.databinding.FragmentSecondReviewBinding
+import com.yoochangwons.kotlinmissingcode.databinding.FragmentSecondReviewItemBinding
 
 class FragmentSecondReview : Fragment() {
 
@@ -30,4 +35,34 @@ class FragmentSecondReview : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+}
+
+data class FragmentTalentList(val text: String)
+
+class FragmentSecondRecyclerViewAdapter(private val dataSet: List<FragmentTalentList>) :
+    RecyclerView.Adapter<FragmentSecondRecyclerViewAdapter.ViewHolder>() {
+
+    class ViewHolder(val itemBinding: FragmentSecondReviewItemBinding) : RecyclerView.ViewHolder(itemBinding.root) {}
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.fragment_second_review_item, viewGroup, false)
+
+        return ViewHolder(FragmentSecondReviewItemBinding.bind(view))
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemBinding.frSecondTextView.text = dataSet[position].text
+    }
+
+    override fun getItemCount() = dataSet.size
+
+}
+
+class FragmentSecondViewModel : ViewModel() {
+    val talentLiveData = MutableLiveData<List<FragmentTalentList>>()
+
+    private val talentList = ArrayList<FragmentTalentList>()
+
+
 }
