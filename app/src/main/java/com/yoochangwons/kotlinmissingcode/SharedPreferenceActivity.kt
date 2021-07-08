1296b5f2ac12dae013f97fac1f898ae8d240e7f8
CN <ckddnjs554@naver.com>
2021-07-08 15:53:47 +0900

    Feat : SharePreference save, select, delete, delete 기능 구현

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/SharedPreferenceActivity.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/SharedPreferenceActivity.kt
@@ -20,7 +20,6 @@ class SharedPreferenceActivity : AppCompatActivity() {
         setContentView(view)
 
         // SharePreference 가져온 후 저장하는 방법
-        val sharedPreference = getSharedPreferences("sp1", Context.MODE_PRIVATE)
 
     // Mode
         // - MODE_PRIVATE : 생성한 application 에서만 사용 가능
@@ -29,20 +28,44 @@ class SharedPreferenceActivity : AppCompatActivity() {
 
         // SharePreference 데이터 주입방법
         // editor 를 통해서 data 주입
-        val editor: SharedPreferences.Editor = sharedPreference.edit()
-        editor.putString("hello", "안녕하세요")
-        editor.commit()
+//        val editor: SharedPreferences.Editor = sharedPreference.edit()
+//        editor.putString("hello", "안녕하세요")
+//        editor.commit()
 
         // sp1 -> SharedPreference
         //        (Key-Value) -> ("Hello", "안녕하세요")
         // sp2 -> SharedPreference
         //        (Key-Value) -> ("Hello", "안녕하세요11")
 
-        binding.spButton.setOnClickListener {
+        binding.saveButton.setOnClickListener {
+            val sharedPreference = getSharedPreferences("sp1", Context.MODE_PRIVATE)
+            val editor: SharedPreferences.Editor = sharedPreference.edit()
+            editor.putString("hello", "안녕하세요")
+            editor.putString("goodbye", "안녕히가세요")
+            editor.apply()
+        }
+
+        binding.selectedButton.setOnClickListener {
             // SharePreference 값을 불러오는 방법
             val sharedPreference = getSharedPreferences("sp1", Context.MODE_PRIVATE)
-            val value = sharedPreference.getString("hello", "데이터 없음")
-            Log.d("key-value", "Value : $value")
+            val value1 = sharedPreference.getString("hello", "데이터 없음")
+            val value2 = sharedPreference.getString("goodbye", "데이터 없음")
+            Log.d("key-value", "Value 1 : $value1")
+            Log.d("key-value", "Value 2 : $value2")
+        }
+
+        binding.deleteButton.setOnClickListener {
+            val sharedPreference = getSharedPreferences("sp1", Context.MODE_PRIVATE)
+            val editor = sharedPreference.edit()
+            editor.remove("hello")
+            editor.apply()
+        }
+
+        binding.deleteAllButton.setOnClickListener {
+            val sharedPreference = getSharedPreferences("sp1", Context.MODE_PRIVATE)
+            val editor = sharedPreference.edit()
+            editor.clear()
+            editor.apply()
         }
     }
 }
