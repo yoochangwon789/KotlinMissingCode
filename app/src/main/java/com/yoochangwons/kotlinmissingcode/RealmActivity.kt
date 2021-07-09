8a7454772fea1df7e4870df52b8f0ccc4ac22990
CN <ckddnjs554@naver.com>
2021-07-09 15:27:38 +0900

    Feat : realm 초기화 작업

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/RealmActivity.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/RealmActivity.kt
@@ -2,12 +2,19 @@ package com.yoochangwons.kotlinmissingcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class RealmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_realm)
 
-
+        // Realm 초기화 작업
+        Realm.init(this)
+        val config: RealmConfiguration = RealmConfiguration
+            .Builder()
+            .deleteRealmIfMigrationNeeded()
+            .build()
     }
 }
