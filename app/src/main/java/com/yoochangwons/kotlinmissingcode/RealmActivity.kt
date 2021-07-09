6d212e43a65ec49bfecae2aaa80a3a0e1198d660
CN <ckddnjs554@naver.com>
2021-07-09 15:23:34 +0900

    Feat : realm activity xml 디자인 구성

---
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/RealmActivity.kt
@@ -0,0 +1,13 @@
+package com.yoochangwons.kotlinmissingcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RealmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_realm)
+
+
+    }
+}
