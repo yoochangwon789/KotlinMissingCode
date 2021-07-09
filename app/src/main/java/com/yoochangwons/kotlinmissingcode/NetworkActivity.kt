74a6e2df3fc56637d493d4dbc891717555fc3e10
CN <ckddnjs554@naver.com>
2021-07-09 20:30:02 +0900

    Feat : Network connection 초기 코드 작성

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/NetworkActivity.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/NetworkActivity.kt
@@ -2,10 +2,36 @@ package com.yoochangwons.kotlinmissingcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class NetworkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_network)
+
+        val urlString: String = "http://mellowcode.org/json/student"
+        val url: URL = URL(urlString)
+        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+        connection.requestMethod = "GET"
+        // Header 작성
+        // Key -> Content-Type
+        // value -> application/json
+        connection.setRequestProperty("Content-Type", "application/json")
+
+        var buffer = ""
+        if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+            Log.d("connn", "inputstream : ${connection.inputStream}")
+            val reader = BufferedReader(
+                InputStreamReader(
+                    connection.inputStream, "UTF-8"
+                )
+            )
+            buffer = reader.readLine()
+        }
     }
 }
