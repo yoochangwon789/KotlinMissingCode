6a3cbc9864466caa369e5913b1117f52900fc139
CN <ckddnjs554@naver.com>
2021-07-10 18:34:04 +0900

    Feat : NetWork Async 클래스 구현

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/NetWorkRecyclerViewActivity.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/NetWorkRecyclerViewActivity.kt
@@ -8,6 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
 import java.net.URL
 
 class NetWorkRecyclerViewActivity : AppCompatActivity() {
@@ -23,6 +28,24 @@ class NetWorkRecyclerViewTask : AsyncTask<Any?, Any?, Any?>() {
     override fun doInBackground(vararg params: Any?): Any? {
         val urlString = "http://mellowcode.org/json/students/"
         val url : URL = URL(urlString)
+        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+        connection.requestMethod = "GET"
+        connection.setRequestProperty("Content-Type", "application/json")
+
+        var buffer = ""
+        if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+            val reader = BufferedReader(
+                InputStreamReader(
+                    connection.inputStream, "UTF-8"
+                )
+            )
+            buffer = reader.readLine()
+        }
+
+        val jsonData = Gson().fromJson(buffer, Array<PersonFromServer>::class.java)
+
+        return null
     }
 }
 
