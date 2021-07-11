53ba9d7d273badb09f600fe561332836caa53855
CN <ckddnjs554@naver.com>
2021-07-11 15:36:38 +0900

    Feat : RetrofitService 인터페이스 생성 후 GET 방식의 Mapping 구현

---
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/RetrofitService.kt
@@ -0,0 +1,14 @@
+package com.yoochangwons.kotlinmissingcode
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+// API 가 변하는 부분을 관리해 주면 된다
+// interface 으로 만드는 이유은 retrofit 의 설명서 양식이기 때문
+interface RetrofitService {
+    
+    // 원하는 Mapping -> Get 을 만들어주고 함수를 생성 후 원하는 return 타입을 설정
+    // call 이라는 제너리타입 안에 return 타입을 설정
+    @GET("json/student/")
+    fun getStudentsList(): Call<ArrayList<PersonFromServer>>
+}
