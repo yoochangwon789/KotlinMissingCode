cc5322a3ea00b943973826c0243dea21256b5824
CN <ckddnjs554@naver.com>
2021-07-12 21:34:16 +0900

    Feat : SNS 로그인 기능 기본 디자인 구성

---
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/EmailSignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.yoochangwons.kotlinmissingcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailSignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_sign_up)
+    }
+}
