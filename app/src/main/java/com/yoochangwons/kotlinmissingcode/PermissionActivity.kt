9e25425c1f128af61b85d565fbd973079d3c01d3
CN <ckddnjs554@naver.com>
2021-07-12 20:44:46 +0900

    Feat : Camera permission 추가

---
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/PermissionActivity.kt
@@ -0,0 +1,13 @@
+package com.yoochangwons.kotlinmissingcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PermissionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_permission)
+
+
+    }
+}
