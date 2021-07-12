39e7d831a5e7d7bbd1ec7981d6e134a8cc51d8ba
CN <ckddnjs554@naver.com>
2021-07-12 19:45:48 +0900

    Feat : Retrofit POST 방식 데이터 저장 및 데이터 불러오기

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/RetrofitActivity.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/RetrofitActivity.kt
@@ -25,6 +25,7 @@ class RetrofitActivity : AppCompatActivity() {
         val service = retrofit.create(RetrofitService::class.java)
 
         // enqueue 의 CallBack 함수를 통해 응답을 받는다
+        // GET 요청
         service.getStudentsList().enqueue(object : Callback<ArrayList<PersonFromServer>> {
             override fun onResponse(
                 call: Call<ArrayList<PersonFromServer>>,
@@ -47,6 +48,26 @@ class RetrofitActivity : AppCompatActivity() {
 
             override fun onFailure(call: Call<ArrayList<PersonFromServer>>, t: Throwable) {
                 Log.d("retrofitt" , "ERROR")
+            }
+        })
+
+        // POST 요청
+        val params = HashMap<String, Any>()
+        params.put("name", "아이유")
+        params.put("age", 20)
+        params.put("intro", "안녕하세요")
+        service.createStudent(params).enqueue(object : Callback<PersonFromServer> {
+            override fun onResponse(
+                call: Call<PersonFromServer>,
+                response: Response<PersonFromServer>
+            ) {
+                if (response.isSuccessful) {
+                    val person = response.body()
+                    Log.d("retrofitt", "name : ${person?.name}")
+                }
+            }
+
+            override fun onFailure(call: Call<PersonFromServer>, t: Throwable) {
             }
         })
 
