5d3b540071b6eab4d613965d4f9a399f626f1324
CN <ckddnjs554@naver.com>
2021-07-12 20:59:41 +0900

    Feat : Permission 승락 거부 버튼 추가

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/PermissionActivity.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/PermissionActivity.kt
@@ -6,28 +6,31 @@ import android.os.Bundle
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_permission.*
 
 class PermissionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission)
 
-        // 권한이 이미 부여되어 있는 경우를 확인하는 법
-        val camaraPermissionCheck = ContextCompat.checkSelfPermission(
-            this,
-            android.Manifest.permission.CAMERA
-        )
-
-        if (camaraPermissionCheck != PackageManager.PERMISSION_GRANTED) {
-            // 권한이 없는 경우
-            ActivityCompat.requestPermissions(
+        ask.setOnClickListener {
+            // 권한이 이미 부여되어 있는 경우를 확인하는 법
+            val camaraPermissionCheck = ContextCompat.checkSelfPermission(
                 this,
-                arrayOf(android.Manifest.permission.CAMERA),
-                1000
+                android.Manifest.permission.CAMERA
             )
-        } else {
-            // 권한이 있는 경우
-            Log.d("permissionss", "권한이 이미 있음")
+
+            if (camaraPermissionCheck != PackageManager.PERMISSION_GRANTED) {
+                // 권한이 없는 경우
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.CAMERA),
+                    1000
+                )
+            } else {
+                // 권한이 있는 경우
+                Log.d("permissionss", "권한이 이미 있음")
+            }
         }
     }
 
