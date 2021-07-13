7361fd49fddbe255debbe2d2bab43ae565e3cae2
CN <ckddnjs554@naver.com>
2021-07-13 19:34:02 +0900

    Feat : PostList RecyclerView 관한 xml 디자인 구성

---
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/OutStagramPostListActivity.kt
@@ -0,0 +1,11 @@
+package com.yoochangwons.kotlinmissingcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OutStagramPostListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_out_stagram_post_list)
+    }
+}
