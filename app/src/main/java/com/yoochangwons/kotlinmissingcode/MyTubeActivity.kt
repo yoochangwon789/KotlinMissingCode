6775b83153734b6666e60ffa4345abda01f80c84
CN <ckddnjs554@naver.com>
2021-07-16 16:20:03 +0900

    Feat : MyTube 의 YouTube list item 디자인 구현

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/MyTubeActivity.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/MyTubeActivity.kt
@@ -2,10 +2,47 @@ package com.yoochangwons.kotlinmissingcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
 
 class MyTubeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_tube)
     }
+}
+
+class MyTubeAdapter(
+    private val dataSet: ArrayList<YouTube>,
+    private val glide: RequestManager
+) : RecyclerView.Adapter<MyTubeAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var title: TextView? = null
+        private val thumbnail: ImageView
+        var content: TextView? = null
+
+        init {
+            title = itemView.findViewById(R.id.item)
+            thumbnail = itemView.findViewById(R.id.post_img)
+            content = itemView.findViewById(R.id.post_content)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.youtube_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+    }
+
+    override fun getItemCount() = dataSet.size
 }
