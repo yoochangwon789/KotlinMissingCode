63fa356b5d4dfa1ea30d80525ad899f7a6f883be
CN <ckddnjs554@naver.com>
2021-07-16 16:47:38 +0900

    Feat : MyTube recyclerView 구현 완료

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/MyTubeActivity.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/MyTubeActivity.kt
@@ -2,18 +2,51 @@ package com.yoochangwons.kotlinmissingcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
+import kotlinx.android.synthetic.main.activity_my_tube.*
+import kotlinx.android.synthetic.main.activity_out_stagram_my_post_list.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyTubeActivity : AppCompatActivity() {
+
+    lateinit var glide: RequestManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_tube)
+
+
+        (application as MasterApplication).service.getYoutubeList().enqueue(
+            object : Callback<ArrayList<YouTube>> {
+                override fun onResponse(
+                    call: Call<ArrayList<YouTube>>,
+                    response: Response<ArrayList<YouTube>>
+                ) {
+                    if (response.isSuccessful) {
+                        glide = Glide.with(this@MyTubeActivity)
+                        val youtubeList = response.body()
+                        val adapter = MyTubeAdapter(youtubeList!!, glide)
+                        my_tube_recycler_view.adapter = adapter
+                        my_tube_recycler_view.layoutManager = LinearLayoutManager(this@MyTubeActivity)
+                    }
+                }
+
+                override fun onFailure(call: Call<ArrayList<YouTube>>, t: Throwable) {
+                    Log.d("1234", "error")
+                }
+
+            })
     }
 }
 
