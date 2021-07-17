7366bb9ee069ef263ae73daa0cb83cf460ae3ee4
CN <ckddnjs554@naver.com>
2021-07-17 17:17:39 +0900

    Feat : Song Class 객체 Serializable 작성

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/Song.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/Song.kt
@@ -1,7 +1,9 @@
 package com.yoochangwons.kotlinmissingcode
 
+import java.io.Serializable
+
 class Song(
     var title: String? = null,
     var thumbnail: String? = null,
     var song: String? = null
-)
+) : Serializable
