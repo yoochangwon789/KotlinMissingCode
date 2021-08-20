928d84983f624da4bc8648dc2c1def2c34f8df33
CN <ckddnjs554@naver.com>
2021-08-20 17:55:19 +0900

    Feat : LifeCycleTestActivity 라이프싸이클 테스트 코드 작성

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/LifeCycleTestActivity.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/LifeCycleTestActivity.kt
@@ -2,10 +2,38 @@ package com.yoochangwons.kotlinmissingcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class LifeCycleTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_cycle_test)
+
+        Log.d("LifeCycleTest", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("LifeCycleTest", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("LifeCycleTest", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LifeCycleTest", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LifeCycleTest", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LifeCycleTest", "onDestroy")
     }
 }
