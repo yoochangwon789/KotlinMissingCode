a27044b7c368122ec33f420bd77ef9d15dd1c752
CN <ckddnjs554@naver.com>
2021-08-21 16:20:00 +0900

    Feat : FragmentLifeCycleActivity fragment 클래스 라이프 사이클 메서드 오버라이딩 작업

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/FragmentLifeCycle.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/FragmentLifeCycle.kt
@@ -1,7 +1,51 @@
 package com.yoochangwons.kotlinmissingcode
 
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class FragmentLifeCycle: Fragment() {
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
 }
