e93082773112ecd50ad1bfbf132eee3c3bd17535
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-02-16 01:47:28 +0900

    Feat : Comparable Generic 함수 테스트 코드 작성

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/TestCode/GenericTestCode.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/TestCode/GenericTestCode.kt
@@ -6,9 +6,25 @@ class Rectangle<T>(private val width: T, private val height: T) where T: Number,
         return (width.toDouble() * height.toDouble()) as T
     }
 
+    fun compare(): Boolean {
+        return width < height
+    }
+}
+
+fun <T: Comparable<T>> compare(a: T, b: T): Boolean {
+    return a < b
 }
 
 fun main(args: Array<String>) {
 
-    print(Rectangle(10, 20).getArea())
+    println(Rectangle(10, 20).getArea())
+    println(Rectangle(10, 20).compare())
+
+    println("-------------------------------------")
+
+    println(compare(1, 2))
+    println(compare(2, 1))
+    println(compare("aaaa", "aaab"))
+    println(compare("1", "2"))
+    println(compare("cat", "dog"))
 }
