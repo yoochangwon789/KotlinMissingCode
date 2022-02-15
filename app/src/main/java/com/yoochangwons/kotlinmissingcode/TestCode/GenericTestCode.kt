4ba92d5cdd4c9a0cd7490d3543fa6860e4a0f636
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-02-16 00:49:06 +0900

    Feat : Generic where 테스트 구문 작성

--- app/src/main/java/com/yoochangwons/kotlinmissingcode/TestCode/GenericTestCode.kt
+++ app/src/main/java/com/yoochangwons/kotlinmissingcode/TestCode/GenericTestCode.kt
@@ -1,2 +1,14 @@
 package com.yoochangwons.kotlinmissingcode.TestCode
 
+class Rectangle<T>(private val width: T, private val height: T) where T: Number, T: Comparable<T> {
+
+    fun getArea(): T {
+        return (width.toDouble() * height.toDouble()) as T
+    }
+
+}
+
+fun main(args: Array<String>) {
+
+    print(Rectangle(10, 20).getArea())
+}
